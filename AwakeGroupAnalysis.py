1027bade458b11eeb5a16408adae63b414f8a3d2
Valeria <valeria.amato@mail.polimi.it>
2022-04-14 12:29:39 +0200

    Filter individuated
    90.
    Also 60 can be good sometimes

--- AwakeGroupAnalysis.py
+++ AwakeGroupAnalysis.py
@@ -1,6 +1,7 @@
 
 # The goal is to find the best filter window.
 # When can we be sure of the awake action?
+import math
 
 import pandas as pd
 import numpy
@@ -9,35 +10,39 @@ from SleepQuality.AwakeFunction import awakeIntoGroups
 from SleepQuality.AuxiliaryFunction import createNewStatusList
 
 csvFile = []
-csvFile.append("..\Archivio\EachSecond2\\bed_raw_2022-04-01_60-F1-89-29-82-44_elaborated90.csv")
-csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-02_60-F1-89-29-82-44_elaborated90.csv")
-csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-05_60-F1-89-29-82-44_elaborated90.csv")
-csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-24_60-F1-89-29-82-44_elaborated90.csv")
-csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-25_60-F1-89-29-82-44_elaborated90.csv")
+csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-24_60-F1-89-29-82-44_elaborated.csv")
+csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-25_60-F1-89-29-82-44_elaborated.csv")
+csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-26_60-F1-89-29-82-44_elaborated.csv")
+csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-27_60-F1-89-29-82-44_elaborated.csv")
+csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-28_60-F1-89-29-82-44_elaborated.csv")
 
 windowToTest = [240, 180, 150, 120, 100, 90, 60, 45, 30, 20, 15, 10, 5]
 statusGroup = createNewStatusList(windowToTest)
-numAwakeGroups=[]
-for numFile in range(4):
+completeStatusGroup = list(statusGroup.values())
+completeStatusGroup = list(map(str, completeStatusGroup))
+completeStatusGroup.insert(0, "Original")
+for numFile in range(5):
   print(csvFile[numFile])
+  numAwakeGroups = []
   data = pd.read_csv(csvFile[numFile], sep=",")
 
   statusLevel = 2
   awakeGroups= awakeIntoGroups(data, "status")
+  numAwakeGroups.append(len(awakeGroups))
 
   statusPoints = numpy.copy(data.loc[data["status"] == 0].index)
   statusLevelPoints = ["status"] * statusPoints.size
   plt.scatter(statusPoints, statusLevelPoints, marker=".")
   for newStatus, windowDim in statusGroup.items():
     awakeGroups = awakeIntoGroups(data, newStatus)
+    numAwakeGroups.append(len(awakeGroups))
 
     statusLevel += 2
     statusPoints = numpy.copy(data.loc[data[newStatus] == 0].index)
     statusLevelPoints = [newStatus] * statusPoints.size
-    plt.scatter(statusPoints, statusLevelPoints, marker=".")
 
-  plt.title(csvFile[numFile])
-  plt.xlabel("Time")
+  plt.ylabel("Number of awake groups")
+  plt.title("Comparison of number of awake groups")
+  print(numAwakeGroups)
+  plt.bar(completeStatusGroup, numAwakeGroups)
   plt.show()
-
-
--- DataPreparation.py
+++ DataPreparation.py
@@ -5,7 +5,7 @@ from SleepQuality.AuxiliaryFunction import createNewStatusList
 
 def outputFileName(file):
   file = file[:-4]
-  file = file + "_elaborated.csv"
+  file = file + "_elaborated90.csv"
   return file
 
 csvFile = []
@@ -14,11 +14,20 @@ csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-25_60-F1-89-29-82-44.csv
 csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-26_60-F1-89-29-82-44.csv")
 csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-27_60-F1-89-29-82-44.csv")
 csvFile.append("..\Archivio\EachSecond\\bed_raw_2022-03-28_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond2\\bed_raw_2022-03-29_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond2\\bed_raw_2022-03-30_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond2\\bed_raw_2022-03-31_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond2\\bed_raw_2022-04-01_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-01_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-02_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-03_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-04_60-F1-89-29-82-44.csv")
+csvFile.append("..\Archivio\EachSecond3\\bed_raw_2022-04-05_60-F1-89-29-82-44.csv")
 
-windowToTest = [120, 90, 60, 30, 20, 15, 10, 5]
+windowToTest = [240, 180, 150, 120, 100, 90, 60, 45, 30, 20, 15, 10, 5]
 statusGroup = createNewStatusList(windowToTest)
 
-for numFile in range(5):
+for numFile in range(10,14):
   print("From:", csvFile[numFile])
   print("To: ", outputFileName(csvFile[numFile]))
   data = pd.read_csv(csvFile[numFile], sep=";")
@@ -30,7 +39,7 @@ for numFile in range(5):
 
   for newStatus, windowDim in statusGroup.items():
     print("Elaborating", newStatus, "...")
-    data[newStatus]=data.apply(lambda row: reportLawStatus(data, row, windowDim, 0.8), axis=1)
+    data[newStatus]=data.apply(lambda row: reportLawStatus(data, row, windowDim, 0.9), axis=1)
 
   data.to_csv(outputFileName(csvFile[numFile]), encoding='utf-8')
 
